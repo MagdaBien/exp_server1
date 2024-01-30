da530a7107024c1db0af4fe6e662821e90f8b3fe
Magdalena Bień <bien@magda.krakow.pl>
2024-01-30 20:59:44 +0100

    adding template

--- server.js
+++ server.js
@@ -1,35 +1,47 @@
+const hbs = require("express-handlebars");
 const express = require("express");
 const path = require("path");
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-app.get("/", (req, res) => {
-  res.show("index.html");
-});
-
-app.get("/home", (req, res) => {
-  res.show("index.html");
-});
-
-app.get("/about", (req, res) => {
-  res.show("about.html");
-});
-
-app.use("/user", (req, res, next) => {
-  res.show("register.html");
-});
+app.engine(".hbs", hbs());
+app.set("view engine", ".hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+app.get("/", (req, res) => {
+  res.render("index", { layout: "main_dark" });
+});
+
+app.get("/home", (req, res) => {
+  res.render("index");
+});
+
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { name: req.params.name });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/info", (req, res) => {
+  res.render("info");
+});
+
+app.get("/history", (req, res) => {
+  res.render("history");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.use("/user", (req, res, next) => {
+  res.render("register");
+});
+
 app.use((req, res) => {
-  res.show("error404.html");
+  res.render("error404");
 });
 
 app.listen(8000, () => {
