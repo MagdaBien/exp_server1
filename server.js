2ff63ca81d623edeb1c192a6d6b2b31a61054599
Magdalena Bień <bien@magda.krakow.pl>
2024-02-04 11:45:36 +0100

    adding file

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const hbs = require("express-handlebars");
 const express = require("express");
 const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
 const app = express();
 app.engine(".hbs", hbs());
 app.set("view engine", ".hbs");
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg" ||
+      file.mimetype == "image/gif"
+    ) {
+      cb(null, true);
+    } else {
+      return cb(new Error("Invalid mime type"));
+    }
+  },
+});
+
+const uploadSingleImage = upload.single("design");
+
 app.get("/", (req, res) => {
-  res.render("index", { layout: "main_dark" });
+  res.render("index");
 });
 
 app.get("/home", (req, res) => {
@@ -38,6 +67,25 @@ app.get("/about", (req, res) => {
 
 app.use("/user", (req, res, next) => {
   res.render("register");
+});
+
+app.post("/contact/send-message", function (req, res) {
+  uploadSingleImage(req, res, function (err) {
+    const { author, sender, title, message } = req.body;
+
+    if (err) {
+      return res.render("contact", { isError2: true });
+    }
+
+    if (author && sender && title && message && req.file) {
+      res.render("contact", {
+        isSent: true,
+        fileName: req.file.originalname,
+      });
+    } else {
+      res.render("contact", { isError: true });
+    }
+  });
 });
 
 app.use((req, res) => {
